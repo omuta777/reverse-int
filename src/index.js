a0eb3516e07b5827cbc433bd123044a04ff70a83
Victoria Okosyan <resemble@rambler.ru>
2022-02-03 23:52:29 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+let str1 = n.toString();
+let arr1 = str1.split('');
+let arr2 = arr1.reverse();
+let str2 = arr2.join('');
+let newNum =  parseInt(str2);
+return newNum;
 }
